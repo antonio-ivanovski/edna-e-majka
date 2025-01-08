9e9af64ab1ba041829890fb09ce6c5f8ac671021
Antonio Ivanovski <antonio-ivanovski@users.noreply.github.com>
2025-01-08 22:44:37 +0100

    try node fix

--- next.config.js
+++ next.config.js
@@ -20,6 +20,7 @@ module.exports = (phase, { defaultConfig }) => {
       // Fixes npm packages that depend on `fs` module
       if (!isServer) {
         config.node = {
+          ...config.node,
           fs: 'empty' // https://github.com/vercel/next.js/issues/7755
         }
       }
