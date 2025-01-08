ba6ba63a0190bc9180fe61a2a924dfe51571faf6
Antonio Ivanovski <antonio-ivanovski@users.noreply.github.com>
2025-01-08 22:56:14 +0100

    revert custom webpack config

--- next.config.js
+++ next.config.js
@@ -15,6 +15,17 @@ module.exports = (phase, { defaultConfig }) => {
           permanent: false
         }
       ]
+    },
+    webpack: (config, { isServer }) => {
+      // Fixes npm packages that depend on `fs` module
+      if (!isServer) {
+        config.node = {
+          ...config.node,
+          fs: 'empty' // https://github.com/vercel/next.js/issues/7755
+        }
+      }
+
+      return config
     }
   }
 }
